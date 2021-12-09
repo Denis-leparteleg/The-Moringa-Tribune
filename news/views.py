4564fcc3c144878044125632e4cab1ad9db01080
Denis-leparteleg <denislepartelegke@gmail.com>
2021-12-09 19:16:55 +0300

    Create welcome.html template file

--- news/views.py
+++ news/views.py
@@ -6,7 +6,7 @@ from django.http  import HttpResponse,Http404
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
