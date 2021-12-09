d38c3d8a5d0d26d2300c593e69d1a71f53b59098
Denis-leparteleg <denislepartelegke@gmail.com>
2021-12-09 19:30:13 +0300

    Update past_days_news view function

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
 from django.http  import HttpResponse,Http404
@@ -13,20 +13,19 @@ def news_of_day(request):
     return render(request, 'all-news/today-news.html', {"date": date,})
 
 
-def past_days_news(request,past_date):
-    
+# View Function to present news from past days
+def past_days_news(request, past_date):
+
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-        day = convert_dates(date)
-        html = f'''
-            <html>
-                <body>
-                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-                </body>
-            </html>
-                '''
-        return HttpResponse(html)
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
